f171780b64423b9af1af85f8e19349137e8724a0
itsmgo <marti@merceorti.com>
2026-06-09 09:57:03 +0200

    added extra performance metrics and validation with confidence intervals to misinformation model

--- misinformation-diffusion-repast-hpc/tools/plot_network.py
+++ misinformation-diffusion-repast-hpc/tools/plot_network.py
@@ -15,11 +15,11 @@ if __name__ == "__main__":
     arg_parser.add_argument("--network", type=str, default="./networks/ws__k_100_p_0.5_network_1000.gml", help="Path to the GML file of the network to visualize")
     args = arg_parser.parse_args()
 
-    # G = nx.read_gml(args.network)
+    G = nx.read_gml(args.network)
     # G = nx.barabasi_albert_graph(1000, 10)
     # G = nx.erdos_renyi_graph(1000, 0.1)
     # G = nx.watts_strogatz_graph(1000, 100, 0.01)
-    G = nx.powerlaw_cluster_graph(n=10000, m=2, p=0.9)
+    # G = nx.powerlaw_cluster_graph(n=10000, m=2, p=0.9)
 
     # print avg degree and cluster coefficient
     avg_degree = sum(dict(G.degree()).values()) / G.number_of_nodes()
@@ -39,16 +39,15 @@ if __name__ == "__main__":
     
 
     # plot network nodes
-    # plt.figure(figsize=(12, 12))
-    # plt.title("GML Network Visualization", fontsize=14, fontweight="bold")
-    #
-    # pos = nx.spring_layout(G, seed=42)
-    # nx.draw_networkx_nodes(
-    #     G, pos, node_size=700, node_color="skyblue", edgecolors="black"
-    # )
-    # nx.draw_networkx_edges(G, pos, width=2, alpha=0.6, edge_color="gray")
-    # nx.draw_networkx_labels(G, pos, font_size=12, font_family="sans-serif")
-    #
-    # plt.axis("off")
-    # plt.tight_layout()
-    # plt.show()
+    plt.figure(figsize=(12, 12))
+    plt.title("GML Network Visualization", fontsize=14, fontweight="bold")
+
+    pos = nx.spring_layout(G, seed=42)
+    nx.draw_networkx_nodes(
+        G, pos, node_size=700, node_color="skyblue", edgecolors="black"
+    )
+    nx.draw_networkx_edges(G, pos, width=2, alpha=0.6, edge_color="gray")
+
+    plt.axis("off")
+    plt.tight_layout()
+    plt.show()
